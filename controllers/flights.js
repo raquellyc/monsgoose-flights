c0a1c0fb3caf26b8767f8e05ca2c383209275a09
raquellyc <raquels.castillo.78@gmail.com>
2022-01-14 18:08:51 -0700

    work on the add flight function

--- controllers/flights.js
+++ controllers/flights.js
@@ -1,22 +1,25 @@
 const Flight = require('../models/flight')
 
 module.exports = {
-    new: newflight,
-    create
+    new: newFlight,
+    create,
 };
-
-function newFlight(req, res) {
-    res.render('flights/new');
-}
 
 function create(req, res) {
     req.body.nowShowing =!!req.body.nowshowing;
     req.body.cast = req.body.cast.trim();
     if (req.body.cast) req.body.cast = req.body.cast.split(/\s*,\s*/);
+    for (let key in req.body) {
+        if (req.body[key] === '') delete req.body[key];
+    }
     const flight = new Flight(req.body);
     flight.save(function(err) {
         if (err) return res.render('flights/new');
         console.log(flight);
-        res.redirect('/movies/new')
-    })
+        res.redirect('/flights')
+    });
+}
+
+function newFlight(req, res) {
+    res.render('flights/new');
 }
